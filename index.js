0cf9ed23b8a3d8f6431763708d3e390bc02fe070
wiutstudent2024 <102838320+wiutstudent2024@users.noreply.github.com>
2022-04-04 14:36:31 +0500

    Delete uncompleted task

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const { resolveSoa } = require('dns');
 const express = require('express');
 const { json } = require('express/lib/response');
 const app = express();
-const fs = require('fs');
-const { format } = require('path');
-const filePath = './data.json';
+const sqlite3 = require('sqlite3').verbose();
+
+// const fs = require('fs');
+// const { format } = require('path');
+// const filePath = './data.json';
+// const { body, validationResult } = require('express-validator');
+
+//connect to sqlite3 database
+// Creating connection with sqlite3 database
+const db = new sqlite3.Database(
+  './student.db',
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) return console.log(err.message);
+
+    console.log('Connected successfully!');
+  }
+);
+
+// creating tasks table
+// db.run(
+//   'create table tasks (id integer primary key, content varchar(255), done int)'
+// );
 
 app.use('/static', express.static('assets'));
 app.set('view engine', 'pug');
@@ -14,71 +35,55 @@ app.get('/', (req, res) => {
 });
 
 app.get('/tasks', (req, res) => {
-  res.render('tasks');
-});
-// /////////////////////////////////////////
-fs.readFile(filePath, (err, data) => {
-  if (err) throw err;
-  const tasks = JSON.parse(data);
-  console.log(tasks);
-});
-///////////////////////////
-app.post('/tasks/add', (req, res) => {
-  fs.readFile(filePath, (err, data) => {
+  db.all('select * from tasks where done = 0', [], (err, data) => {
     if (err) throw err;
-    const FormData = req.body;
-    const tasks = JSON.parse(data);
-    const task = {
-      id: id(),
-      content: FormData.task,
-      done: false,
-    };
-    console.log(task);
+    res.render('tasks', { tasks: data });
+  });
 
-    tasks.push(task);
+  // res.render('tasks');
+  // });
+});
 
-    fs.writeFile(filePath, JSON.stringify(tasks), (err) => {
-      if (err) throw err;
+// adding a new task
+app.post('/tasks/add', (req, res) => {
+  const formData = req.body;
 
-      fs.readFile(filePath, (err, data) => {
-        if (err) throw err;
-        const tasks = JSON.parse(data);
+  taskInsert = 'insert into tasks(content, done) values (?, ?)';
+  db.run(taskInsert, [formData.task, '0'], (err) => {
+    if (err) throw err;
 
-        res.render('tasks', { tasksData: tasks });
-      });
-    });
-    console.log(FormData.task);
+    console.log('New task added');
 
-    //////////////////
     res.redirect('/tasks');
   });
 });
-// app.post('/tasks/add', (req, res) => {
-//   let FormData = req.body;
-//   fs.readFile(filePath, (err, data) => {
-//     if (err) throw err;
-//     const tasks = JSON.parse(data);
-// const task = {
-//   id: id(),
-//   content: FormData.task,
-//   done: false,
-// };
 
-//     tasks.push(task);
+// deleting uncompleted task
+app.get('/tasks/:id/deleteundone', (req, res) => {
+  id = req.params.id;
+  db.run('DELETE FROM tasks where id=?', id, (err) => {
+    if (err) throw err;
+    res.redirect('/tasks');
+  });
+  // res.render('edit');
+});
 
-//     fs.writeFile(
-//       filePath,
-//       JSON.stringify(tasks, (err) => {
-//         if (err) throw err;
+// deleting completed task
+app.get('/tasks/:id/deletedone', (req, res) => {
+  id = req.params.id;
+  db.run('DELETE FROM tasks where id=?', id, (err) => {
+    if (err) throw err;
+    res.redirect('/complete');
+  });
+});
 
-//         fs.readFile(filePath, (err, data) => {
-//           if (err) throw err;
+//////////////////////////////////////////////////
+// db.run('update tasks set done = 1 where id = 1');
+// db.all('select * from tasks', [], (err, rows) => {
+//   if (err) return console.log(err.message);
 
-//           const tasks = JSON.parse(date);
-//           res.render('tasks', { success: true, tasksData: tasks });
-//         });
-//       })
-//     );
+//   rows.forEach((row) => {
+//     console.log(row);
 //   });
 // });
 
